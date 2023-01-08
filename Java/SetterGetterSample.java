8ff5095f6aa9b8c0eb73122c22ee79fe983de94f
SHOKI-SATO <81621944+SHOKI-SATO@users.noreply.github.com>
2023-01-08 11:55:38 +0900

    Create SetterGetterSample.java

---
+++ Java/SetterGetterSample.java
@@ -0,0 +1,23 @@
+public class Character {
+private String name;
+
+public void setName(String name) {
+this.name = name;
+}
+
+public String getName() {
+return this.name;
+}
+
+}
+
+
+public class Main {
+public static void main(String[] args) {
+Character hero = new Character();
+hero.setName("Hero");
+System.out.println(hero.getName());
+}
+}
+
+
