290afcf2f043ccffec62cfe4e96dd982b3ab1607
SHOKI-SATO <81621944+SHOKI-SATO@users.noreply.github.com>
2023-01-09 14:46:10 +0900

    Create StaticMethodSample.java

---
+++ Java/SampleCode/StaticMethodSample.java
@@ -0,0 +1,16 @@
+//staticメソッドサンプル
+
+public class Main {
+    public static void main(String[] args) {
+        //メソッド呼び出しを変数に格納
+        var result = twice(3);
+        System.out.println(result);
+    }
+
+    //staticメソッドから呼び出す同じクラスのメソッドはstaticメソッドにする必要がある
+    // staticメソッドの呼び出しは「クラス名.メソッド名（引数）」とする（今回は同じクラスのメソッドなのでクラス名は省略可能）
+    static int twice(int x){
+        return x *2;
+    }
+}
+
