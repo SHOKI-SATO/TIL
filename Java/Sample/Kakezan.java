8c8510669c1e7c7d38c633580942bdd32f926be4
SHOKI-SATO <81621944+SHOKI-SATO@users.noreply.github.com>
2023-01-09 11:05:46 +0900

    Update Kakezan.java

--- Java/Sample/Kakezan.java
+++ Java/Sample/Kakezan.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 9; j++) {
-                System.out.printf("%2d | ", i * j );
+                System.out.printf("%2d | ", i * j );    //数値の整形結果が最低２文字になるように左にスペース
             }
             System.out.println();
 
