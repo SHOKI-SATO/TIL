9612cd3178954f6187a753dcd37b374ee82ee880
SHOKI-SATO <81621944+SHOKI-SATO@users.noreply.github.com>
2023-01-09 11:51:40 +0900

    Update ForEachSample.java

--- Java/Sample/ForEachSample.java
+++ Java/Sample/ForEachSample.java
@@ -1,10 +1,21 @@
 //拡張for文
 
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
+        
+        //配列の場合
         var nums = new int[]{2, 3, 5, 7};
         for (int num: nums)  {
             System.out.println(num);
+        }
+        
+        //リストの場合  
+        var strs = List.of("apple", "banana", "grape");
+        for (String str : strs) {
+            System.out.println(str);
 
         }
     }
