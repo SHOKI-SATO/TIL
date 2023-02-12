cd8f3fcee420a8efd9732d8f03fe76fb39ab464b
SHOKI-SATO <81621944+SHOKI-SATO@users.noreply.github.com>
2023-02-12 17:31:26 +0900

    Create InheritSample.java

---
+++ Java/SampleCode/InheritSample.java
@@ -0,0 +1,48 @@
+//継承のサンプル
+
+import java.util.List;
+
+public class Main {
+    static class User{
+        String name;
+
+        public String getName(){
+            return name;
+        }
+    }
+
+    static class Student extends User{
+        int score;
+
+        Student(String name, int score){
+            this.name = name;
+            this.score = score;
+        }
+
+        public int getScore(){
+            return score;
+        }
+
+        static class Teacher extends User{
+            String subject;
+
+            Teacher(String name, String subject){
+                this.name = name;
+                this.subject = subject;
+            }
+
+            public String getSubject(){
+                return subject;
+            }
+        }
+
+        public static void main(String[] args) {
+            //Userオブジェクトを持つListとしているが、継承の場合は「サブクラスはスーパークラスの一種（is-a関係）」となるので、サブクラスであるStudentクラスとTeacherクラスも格納できる
+            List<User> people = List.of(new Student("一郎",80), new Teacher("二郎", "Math"));
+            for (var p : people){
+                System.out.println("こんにちは%sさん" .formatted(p.getName()));
+            }
+        }
+    }
+}
+
