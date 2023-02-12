8baf554266982f85fc128f776344fe166ab90a0e
SHOKI-SATO <81621944+SHOKI-SATO@users.noreply.github.com>
2023-02-12 15:15:31 +0900

    Update InterfaceSample2.java

--- Java/SampleCode/InterfaceSample2.java
+++ Java/SampleCode/InterfaceSample2.java
@@ -1,12 +1,14 @@
-//インターフェースのサンプル
+//インターフェースのサンプル2
 
 import javax.naming.Name;
 import java.util.List;
 
-public class Main {
+public class InterfaceSample2 {
+    //インターフェースを定義（戻り値とメソッド名（必要であれば引数の定義）のみを記載）
     interface Named {
         String name();
     }
+    //インターフェースの実装（implements　の後にインターフェース名を指定）
     record Student(String name, int score) implements Named {
     }
     record Teature(String name, String subject) implements Named {
@@ -14,6 +16,7 @@ public class Main {
 
     public static void main(String[] args) {
         var people = List.of(new Student("一郎", 80), new Teature("二郎", "Math"));
+        //変数pの型を示すとNamedになる
         for (Named p : people){
             var n = p.name();
             System.out.println("こんにちは%sさん".formatted(n));
